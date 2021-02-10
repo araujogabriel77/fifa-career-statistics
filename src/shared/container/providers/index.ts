9a358d812311e7bdf543eae74264275fd8f7b59b
araujogabriel77 <araujogabrielocn@gmail.com>
2021-02-10 01:47:03 -0300

    containers refatorados

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,29 +1,3 @@
-import { container } from 'tsyringe';
-import mailConfig from '@config/mail';
-
-import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
-import IStorageProvider from './StorageProvider/models/IStorageProvider';
-
-import IMailProvider from './MailProvider/models/IMailProvider';
-import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
-import MailgunMailProvider from './MailProvider/implementations/MailgunMailProvider';
-
-import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
-import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
-
-container.registerSingleton<IStorageProvider>(
-  'StorageProvider',
-  DiskStorageProvider
-);
-
-container.registerSingleton<IMailTemplateProvider>(
-  'MailTemplateProvider',
-  HandlebarsMailTemplateProvider
-);
-
-container.registerInstance<IMailProvider>(
-  'MailProvider',
-  mailConfig.driver === 'ethereal' ?
-    container.resolve(EtherealMailProvider) :
-    container.resolve(MailgunMailProvider)
-);
+import './StorageProvider';
+import './MailTemplateProvider';
+import './MailProvider';
